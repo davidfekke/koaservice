fd845f9edac6a7073274d4f01be762784ff057d4
David Fekke <david@fekke.com>
2023-12-16 20:56:42 -0500

    Adding a new record

--- transactions.js
+++ transactions.js
@@ -8,7 +8,8 @@ const transactions = [
     {id: 101, merchant: "Home Depot", description: '2x4s', date: '2023-12-15T11:15:00Z', price: 8.10},
     {id: 102, merchant: "Lows", description: 'Washer', date: '2023-12-15T10:45:00Z', price: 819.75},
     {id: 103, merchant: "Gamestop", description: 'Lego Batman', date: '2023-12-15T09:23:00Z', price: 69.99},
-    {id: 104, merchant: "Apple", description: 'iPad', date: '2023-12-15T08:21:00Z', price: 499.99}
+    {id: 104, merchant: "Apple", description: 'iPad', date: '2023-12-15T08:21:00Z', price: 499.99},
+    {id: 105, merchant: "Ford", description: '2024 Bronco Sport', date: '2023-12-16T08:25:00Z', price: 41000.99}
 ];
 
 // Routes
