50bccd07c30726693c29079b175d26ff2560fe83
David Fekke <davidfekke@Davids-MacBook-Pro.local>
2023-12-17 16:28:13 -0500

    Fix port issue

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ app.use(bodyParser());
 app.use(transactions.routes());
 app.use(transactions.allowedMethods());
 
-app.listen(3000, () => {
+var port = normalizePort(process.env.PORT || '3000');
+
+app.listen(port, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
